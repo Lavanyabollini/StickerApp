d69f3aa53bc1e1af809f5c8f3e287aec6e1069d1
Lavanya <br.lavanya@mindtree.com>
2020-01-06 15:59:13 +0530

    Initial Commit

---
+++ StickerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StickerApp
+//
+//  Created by Lavanya on 06/01/20.
+//  Copyright © 2020 Lavanya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
